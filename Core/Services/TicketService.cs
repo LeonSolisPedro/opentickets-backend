72a4593b5b599715e25e01528568632cdbffdb06
Pedro León <pedro@wintercr.com>
2024-07-31 11:54:41 -0500

    Agregando try catchs en la capa de aplicación

--- Core/Services/TicketService.cs
+++ Core/Services/TicketService.cs
@@ -1,6 +1,7 @@
 using Core.Dto;
 using Core.Entites;
 using Core.Repositories;
+using Microsoft.Extensions.Logging;
 
 namespace Core.Services;
 
@@ -10,63 +11,114 @@ public class TicketService
 
     private readonly IGenericRepository<Solucion> _repoSolucion;
 
-    public TicketService(IGenericRepository<Ticket> genericRepository, IGenericRepository<Solucion> repoSolucion)
+    private readonly ILogger<TicketService> _logger;
+
+    public TicketService(IGenericRepository<Ticket> genericRepository, IGenericRepository<Solucion> repoSolucion, ILogger<TicketService> logger)
     {
         _genericRepository = genericRepository;
         _repoSolucion = repoSolucion;
+        _logger = logger;
     }
 
 
     public async Task<List<Ticket>> GetList()
     {
-        return await _genericRepository.GetList("Computadora,Computadora.Empleado,Solucion");
+        var list = new List<Ticket>();
+        try
+        {
+            list = await _genericRepository.GetList("Computadora,Computadora.Empleado,Solucion");
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Error");
+        }
+        return list;
     }
 
 
     public async Task<Ticket?> GetOrNull(int id)
     {
-        return await _genericRepository.GetOrNull(x => x.Id == id, "Solucion");
+        Ticket? ticket = null;
+        try
+        {
+            ticket = await _genericRepository.GetOrNull(x => x.Id == id, "Solucion");
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Error");
+        }
+        return ticket;
     }
 
 
     public async Task<Response> Create(Ticket ticket)
     {
-        _genericRepository.Create(ticket);
-        await _genericRepository.SaveChanges();
-        return new Response
+        var response = new Response();
+        try
         {
-            Success = true,
-            Message = "Ticket creado con éxito"
-        };
+            _genericRepository.Create(ticket);
+            await _genericRepository.SaveChanges();
+            response.Success = true;
+            response.Message = "Ticket creado con éxito";
+        }
+        catch (Exception e)
+        {
+          _logger.LogError(e, "Error");
+          response.Message = "Error";
+        }
+        return response;
     }
 
 
     public async Task<Response> Edit(Ticket ticket)
     {
-        _genericRepository.Edit(ticket);
-        await _genericRepository.SaveChanges();
-        return new Response
+        var response = new Response();
+        try
         {
-            Success = true,
-            Message = "Ticket editado con éxito"
-        };
+            _genericRepository.Edit(ticket);
+            await _genericRepository.SaveChanges();
+            response.Success = true;
+            response.Message = "Ticket editado con éxito";
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Error");
+            response.Message = "Error";
+        }
+        return response;
     }
 
 
     public async Task<List<Ticket>> GetTicketsPorIdCompu(int idCompu)
     {
-        return await _genericRepository.GetList(x => x.IdComputadora == idCompu, "Solucion");
+        var list = new List<Ticket>();
+        try
+        {
+            list = await _genericRepository.GetList(x => x.IdComputadora == idCompu, "Solucion");
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Error");
+        }
+        return list;
     }
 
 
     public async Task<Response> AgregarSolucion(Solucion solucion)
     {
-        _repoSolucion.Create(solucion);
-        await _genericRepository.SaveChanges();
-        return new Response
+        var response = new Response();
+        try
         {
-            Success = true,
-            Message = "Solución agregada correctamente"
-        };
+            _repoSolucion.Create(solucion);
+            await _genericRepository.SaveChanges();
+            response.Success = true;
+            response.Message = "Solución agregada correctamente";
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e,"Error");
+            response.Message = "Error";
+        }
+        return response;
     }
 }
